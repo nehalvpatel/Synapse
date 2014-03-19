e54a652df382549078e2e7306dad1080e1320f6c
Nehal Patel <nehal@itspatel.com>
2014-03-19 18:44:44 -0500

    Added comments to Handler test and made it somewhat compliant

--- tests/Synapse/HandlerTest.php
+++ tests/Synapse/HandlerTest.php
@@ -1,90 +1,140 @@
 <?php
 
-	class HandlerTest extends \PHPUnit_Framework_TestCase {
-		/**
-		* This is the object that will be tested
-		* @var Synapse
-		*/
-		protected $object;
-		
-		/**
-		* Mock pdo object
-		* @var PDO
-		*/
-		static private $pdo = null;
-		
-		protected function setUp() {
-			$this->pdo = new PDO("mysql:host=localhost;dbname=synapse_tests", "root", "");
-			
-			$this->synapse = new \Synapse\Handler($this->pdo);
-			session_set_save_handler($this->synapse, true);
-			if (session_status() === PHP_SESSION_NONE) { session_start(); }
-		}
-		
-		protected function tearDown() {
-			unset($this->synapse);
-		}
-		
-		public function testOpen() {
-			$this->synapse->open();
-			
-			$row_query = $this->pdo->prepare("SELECT * FROM `sessions` WHERE `ID` = :ID");
-            $row_query->bindValue(":ID", session_id());
-            $row_query->execute();
-            
-			$this->assertEquals(0, $row_query->rowCount());
-		}
-		
-		/*
-		* @depends testOpen
-		*/
-		public function testWrite() {
-			$_SESSION["noodle"] = "6";
-			
-			$this->assertEquals(6, $_SESSION["noodle"]);
-		}
-		
-		/*
-		* @depends testWrite
-		*/
-		public function testRead() {
-			$this->assertEquals(6, $_SESSION["noodle"]);
-		}
-		
-		/*
-		* @depends testRead
-		*/
-		public function testDestroy() {
-			session_destroy();
-            
-			$row_query = $this->pdo->prepare("SELECT * FROM `sessions` WHERE `ID` = :ID");
-            $row_query->bindValue(":ID", session_id());
-            $row_query->execute();
-            
-			$this->assertEquals(0, $row_query->rowCount());
-		}
-		
-		/*
-		* @depends testDestroy
-		*/
-		public function testClose() {
-			$close_response = $this->synapse->close();
-			
-			$this->assertTrue($close_response);
-		}
+/**
+ * HandlerTest Class Doc Comment
+ *
+ * @category Tests
+ * @package  Synapse
+ * @author   Nehal Patel <nehal@itspatel.com>
+ * @license  http://opensource.org/licenses/MIT MIT license
+ * @link     https://packagist.org/packages/nehalvpatel/synapse
+ */
+class HandlerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * This is the object that will be tested
+     * @var Synapse
+     */
+    protected $object;
+    
+    /**
+     * Mock pdo object
+     * @var PDO
+     */
+    static private $_pdo = null;
+    
+    /** Sets up test environment
+    *
+    * @return void
+    */
+    protected function setUp()
+    {
+        $this->_pdo = new PDO("mysql:host=localhost;dbname=synapse_tests", "root", "");
         
-        /*
-		* @depends ?
-		*/
-        public function testGc() {
-            $this->synapse->gc(ini_get("session.gc_maxlifetime") * -1);
-            
-            $row_query = $this->pdo->prepare("SELECT * FROM `sessions` WHERE `ID` = :ID");
-            $row_query->bindValue(":ID", session_id());
-            $row_query->execute();
-            
-            $this->assertEquals(0, $row_query->rowCount());
+        $this->synapse = new \Synapse\Handler($this->_pdo);
+        session_set_save_handler($this->synapse, true);
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
         }
-	}
-
-?>
+    }
+    
+    /** Destroys test environment
+    *
+    * @return void
+    */
+    protected function tearDown()
+    {
+        unset($this->synapse);
+    }
+    
+    /** Tests session open call
+    *
+    * @return void
+    */
+    public function testOpen()
+    {
+        $this->synapse->open();
+        
+        $row_query = $this->_pdo->prepare("SELECT * FROM `sessions` WHERE `ID` = :ID");
+        $row_query->bindValue(":ID", session_id());
+        $row_query->execute();
+        
+        $this->assertEquals(0, $row_query->rowCount());
+    }
+    
+    /**
+     * Tests data write call
+     * 
+     * @depends testOpen
+     *
+     * @return void
+     */
+    public function testWrite()
+    {
+        $_SESSION["noodle"] = "6";
+        
+        $this->assertEquals(6, $_SESSION["noodle"]);
+    }
+    
+    /**
+     * Tests data read call
+     *
+     * @depends testWrite
+     *
+     * @return void
+     */
+    public function testRead()
+    {
+        $this->assertEquals(6, $_SESSION["noodle"]);
+    }
+    
+    /**
+     * Tests session destroy call
+     *
+     * @depends testRead
+     *
+     * @return void
+     */
+    public function testDestroy()
+    {
+        session_destroy();
+        
+        $row_query = $this->_pdo->prepare("SELECT * FROM `sessions` WHERE `ID` = :ID");
+        $row_query->bindValue(":ID", session_id());
+        $row_query->execute();
+        
+        $this->assertEquals(0, $row_query->rowCount());
+    }
+    
+    /**
+     * Tests session close call
+     *
+     * @depends testDestroy
+     *
+     * @return void
+     */
+    public function testClose()
+    {
+        $close_response = $this->synapse->close();
+        
+        $this->assertTrue($close_response);
+    }
+    
+    /**
+     * Tests garbage collection
+     *
+     * @depends testClose
+     *
+     * @return void
+     */
+    public function testGc()
+    {
+        $this->synapse->gc(ini_get("session.gc_maxlifetime") * -1);
+        
+        $row_query = $this->_pdo->prepare("SELECT * FROM `sessions` WHERE `ID` = :ID");
+        $row_query->bindValue(":ID", session_id());
+        $row_query->execute();
+        
+        $this->assertEquals(0, $row_query->rowCount());
+    }
+}
